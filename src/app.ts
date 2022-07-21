c55e663df2f3ca63d78e4f2423d80676a4e11144
NEARworld <artemismars2@outlook.com>
2022-07-21 17:17:00 +0900

    feat: setup unsplash-js api in typescript

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,32 @@
+import "dotenv/config";
 import express, { Request, Response, NextFunction } from "express";
+import { createApi } from "unsplash-js";
+import nodeFetch from "node-fetch";
+import * as config from "./config.js";
 
 const app = express();
 
+// declare global {
+//   var fetch: typeof nodeFetch.default;
+//   type RequestInit = nodeFetch.RequestInit;
+//   type Response = nodeFetch.Response;
+// }
+
+const unsplash = createApi({
+  accessKey: config.accessKey,
+  fetch: nodeFetch as unknown as typeof fetch,
+});
+
 app.get("/", (req: Request, res: Response, next: NextFunction) => {
-  res.send("welcome!");
+  unsplash.photos
+    .getRandom({
+      count: 10,
+    })
+    .then((response: any) => {
+      res.json(response.response);
+    })
+    .catch((err: object) => console.log(err));
+  console.log("get request to /");
 });
 
 app.listen(8080, () => {
---
+++ src/config.ts
@@ -0,0 +1,2 @@
+// unsplash api access key
+export const accessKey: string = process.env.API_ACCESS_KEY!;
